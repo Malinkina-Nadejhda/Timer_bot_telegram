074a643d2ee91210e68ef6d1e2605f78c22ed1a3
Malinkina-Nadejhda <nadekmalek@gmail.com>
2026-02-20 15:26:33 +0300

    Update dvm_lsn_timer_bot.py

--- dvm_lsn_timer_bot.py
+++ dvm_lsn_timer_bot.py
@@ -4,14 +4,6 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv()
-
-TG_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TG_CHAT_ID = os.getenv("TG_CHAT_ID")
-
-bot = ptbot.Bot(TG_TOKEN)
-
-
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
     percent = "{0:.1f}"
@@ -21,30 +13,32 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def notify(chat_id):
+def notify(chat_id, bot):
     bot.send_message(chat_id, "Время вышло")
 
 
-def notify_progress(seconds_left, chat_id, message_id, seconds):
+def notify_progress(seconds_left, chat_id, bot, message_id, seconds):
     seconds_passed = seconds - seconds_left
     progress_bar = render_progressbar(seconds, seconds_passed)
     bot.update_message(chat_id, message_id, f"Осталось {seconds_left} секунд\n {progress_bar}")
 
 
-def timer(chat_id, message):
+def timer(chat_id, message, bot):
     seconds = parse(message)
     start_message = bot.send_message(chat_id, "Таймер запущен")
     message_id = start_message
-    bot.create_countdown(seconds, notify_progress, chat_id=chat_id, message_id=message_id, seconds=seconds)
-    bot.create_timer(seconds, notify, chat_id=chat_id)
+    bot.create_countdown(seconds, notify_progress, bot=bot, chat_id=chat_id, message_id=message_id, seconds=seconds)
+    bot.create_timer(seconds, notify, bot=bot, chat_id=chat_id)
 
 
 def main():
-    bot.reply_on_message(timer)
+    load_dotenv()
+    TG_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    bot = ptbot.Bot(TG_TOKEN)
+    bot.reply_on_message(timer, bot=bot)
     bot.run_bot()
 
 
 if __name__ == '__main__':
     main()
 
-    
